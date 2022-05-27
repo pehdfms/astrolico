615f5334eafdf1105c585e83549482eeb9c073b4
Pedro Henrique <pehdfms@gmail.com>
2022-05-27 12:40:47 -0300

    init repo

---
+++ src/main/java/com/makers/astrolico/AstrolicoApplication.java
@@ -0,0 +1,13 @@
+package com.makers.astrolico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstrolicoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AstrolicoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/makers/astrolico/AstrolicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makers.astrolico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AstrolicoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
