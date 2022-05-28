0e96e20e72dcfbd93a23c875ba76d2c2aa5f7015
Pedro Henrique <pehdfms@gmail.com>
2022-05-28 17:33:00 -0300

    allow CORS

--- src/main/java/com/makers/astrolico/repository/PlanetRepository.java
+++ src/main/java/com/makers/astrolico/repository/PlanetRepository.java
@@ -8,7 +8,9 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 @RepositoryRestResource(collectionResourceRel = "planets", path = "planets")
 public interface PlanetRepository extends JpaRepository<Planet, Long> {
     @Query(
